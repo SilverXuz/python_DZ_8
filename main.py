b6c9f2f9dd9c888b86ae9ebf0189ac060bc4a9b6
SilverXuz <silver-xuz@mail.ru>
2022-10-20 23:23:35 +0300

    бета-тест

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import datetime
 now = datetime.datetime.now()
 now_string = now.strftime('%H:%M:%S %d/%m/%y')
 
+
 # Функция будет записывать логфайл с ошибками.
 def write_to_log(Data: str, filename='error_logfile.txt'):
     with open(filename, 'a') as output:
@@ -12,14 +13,34 @@ def write_to_log(Data: str, filename='error_logfile.txt'):
         output.write('\n')
 
 
-def write_to_directory(contact: dict, filename='directory.csv'):
+def write_to_file(contact: dict, filename='directory.csv'):
     """
     Функция которая производит запись в справочник.
     """
     with open(filename, 'a', newline='', encoding='utf-8') as csvfile:
         title = ['surname', 'name', 'personal_number', 'work_number', 'city', 'comment']
-        writer = csv.DictWriter(csvfile, fieldnames=title)
+        writer = csv.DictWriter(csvfile, fieldnames=title, delimiter=',')
         writer.writerow(contact)
+
+
+def overwrite_file(data: list, filename='directory.csv'):
+    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+        title = ['surname', 'name', 'personal_number', 'work_number', 'city', 'comment']
+        writer = csv.DictWriter(csvfile, fieldnames=title)
+        for row in data:
+            writer.writerow(dict(zip(title, row)))
+
+
+def get_data_from_file(filename='directory.csv') -> list:
+    """
+    Функция которая считывает файл
+    """
+    result = []
+    with open(filename, 'r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            result.append(row)
+    return result
 
 
 def write_contact(value: str) -> dict:
@@ -34,52 +55,54 @@ def write_contact(value: str) -> dict:
         res = input(f'{value}{d[i]}: ')
         res = res.strip()
         result[e[i]] = res
-    print(result)
+    # print(result)
     return result
 
 
-def print_contacts(data) -> 'print':
+def print_all_contacts(data: list) -> 'print':
     """
-    Функция будет выводить все найденные контакты в файле. На вход принимает содержимое всего файла *.csv
+    Функция будет выводить все найденные контакты. На вход принимает содержимое всего файла *.csv
     """
+    print('ID', 'ФАМИЛИЯ', 'ИМЯ', 'ЛИЧНЫЙ ТЕЛЕФОН', 'РАБОЧИЙ ТЕЛЕФОН', 'ГОРОД', 'ПРИМЕЧАНИЕ', sep='\t\t')
     for i, row in enumerate(data):
-        if i == 0:
-            print('ID', 'фамилия', 'имя', 'телефон личный', 'телефон рабочий', 'город', 'примечание', sep='\t')
-        print(i, *row, sep='\t')
+        print(i, *row, sep='\t\t')
 
 
-def print_find_contacts(data, index) -> 'print':
+def print_contacts_by_index(indexes: list):
     """
     Функция будет выводить все строки по индексу с найденными совпадениями поиска
     """
+    data = get_data_from_file()
     for i, row in enumerate(data):
         if i == 0:
             print('ID', 'фамилия', 'имя', 'телефон личный', 'телефон рабочий', 'город', 'примечание', sep='\t')
-        elif i == index:
-            print(i, *row, sep='\t')
+        if i in indexes:
+            print(i, *row, sep='\t\t')
 
 
-def edit_contact(value: str) -> dict:
+def edit_contact(edit_id: str):
     """
     Функция будет вносить измнения в контакт по индексу
     """
-    pass
+    edit_id = int(edit_id)
+    data = get_data_from_file()
+    data.pop(edit_id)
+    overwrite_file(data)
+    new_value = write_contact('Внесите изменения -> ')
+    write_to_file(new_value)
+    print('Контакт перезаписан в справочнике! *(индексы обновлены)*')
 
-def delete_contact(value: str) -> dict:
+
+def delete_contact(del_id: str):
     """
     Функция будет удалять найденный контакт по индексу
     """
-    pass
+    del_id = int(del_id)
+    data = get_data_from_file()
+    data.pop(del_id)
 
 
-def get_data_from_file(filename='directory.csv'):
-    """
-    Функция которая считывает файл
-    """
-    with open(filename, 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        x = [row for row in reader]
-    return x
+    print('Контакт удален из справочника! *(индексы обновлены)*')
 
 
 def text_input(phrase: str) -> str:
@@ -90,24 +113,18 @@ def text_input(phrase: str) -> str:
     result = result.strip()
     return result
 
-# def parse_search_text(value: str) -> int:
-#     """
-#     Функция будет с выбором в главном меню. Если конечно получится перенести сюда.
-#     """
-#     pass
 
-def request_search(value: str, filename='directory.csv') -> int:
+def search(value: str) -> list:
     """
-    Функция будет осуществлять поиск в базе по запросу. И возвращать индекс строки.
+    Функция возвращает список индексов строк, в которых найдена строка value
     """
-    with open(filename) as csvfile:
-        reader = csv.reader(csvfile)
-        for i, row in enumerate(reader):
-            if i == 0:
-                continue
-            for col in row:
-                if value.lower() in col.lower():
-                    return i
+    result = []
+    data = get_data_from_file()
+    for i, row in enumerate(data):
+        for col in row:
+            if value.lower() in col.lower():
+                result.append(i)
+    return result
 
 
 def select_action(value: str) -> int:
@@ -115,8 +132,6 @@ def select_action(value: str) -> int:
     функция будет с выбором в главном меню. Если конечно получится перенести сюда.
     """
     pass
-
-
 
 
 def main():
@@ -132,37 +147,54 @@ def main():
     next_action = 'нажмите клавишу ENTER ...\n'
     search_text = 'Введите данные контакта, который хотите '
     error1 = 'контакт не найден'
+
     while start:
         value_input = 'Введите '
         gen_action = text_input(general_menu)
         if gen_action == '1':
             print('ДОБАВИТЬ КОНТАКТ')
             result = write_contact(value_input)
-            write_to_directory(result)
+            write_to_file(result)
             input(f'Контакт удачно сохранен в справочник, {next_action}')
         elif gen_action == '2':
             print('ИЗМЕНИТЬ КОНТАКТ')
-            result = edit_contact(f'{search_text}изменить:\n') # обращение к поиску
+            result = text_input(f'{search_text}изменить, и посмотрите его ID:\n')
+            contact_indexes = search(result)  # обращение к поиску
+            if len(contact_indexes) > 0:
+                print_contacts_by_index(contact_indexes)
+                edit_id = input("Введите ID контакта, чтобы его изменить:\n")
+                edit_contact(edit_id)
+            else:
+                print(error1)
             text_input(next_action)
         elif gen_action == '3':
             print('УДАЛИТЬ КОНТАКТ')
-            result = edit_contact(f'{search_text}удалить:\n') # обращение к поиску
+            result = text_input(f'{search_text}удалить, и посмотрите его ID:\n')
+            contact_indexes = search(result)  # обращение к поиску
+            if len(contact_indexes) > 0:
+                print_contacts_by_index(contact_indexes)
+                del_id = input("Введите ID контакта, чтобы его удалить:\n")
+                delete_contact(del_id)
+            else:
+                print(error1)
             text_input(next_action)
         elif gen_action == '4':
             print('НАЙТИ КОНТАКТ')
-            result = text_input(f'{search_text}найти:\n')  # обращение к поиску
-            found_contact = request_search(result)
-
-            print_find_contacts(str(get_data_from_file), found_contact)
+            result_input = text_input(f'{search_text}найти:\n')  # обращение к поиску
+            contact_indexes = search(result_input)
+            if len(contact_indexes) > 0:
+                print_contacts_by_index(contact_indexes)
+            else:
+                # write_to_log(data=[result_input, str(error1)])
+                print(error1)
             text_input(next_action)
         elif gen_action == '5':
             print('ВЕСЬ СПРАВОЧНИК')
             data_from_file = get_data_from_file()
-            print_contacts(data_from_file)
+            print_all_contacts(data_from_file)
             text_input(next_action)
         elif gen_action == '6':
             start = False
-
 
 
 if __name__ == '__main__':
